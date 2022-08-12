c59b94e8eb3ad2f6ea59cfc6b2df1d7ccac564d6
actbee <rainman511@outlook.com>
2022-08-11 22:04:42 -0400

    updated page

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -4,23 +4,55 @@ import {ButtonSmall} from '../../editor/button-small';
 import { Link } from "react-router-dom";
 import React from "react";
 import Stack from "@mui/material/Stack";
+import {status} from "../../store";
+import {useRecoilState, useRecoilValue} from "recoil";
+import {READY, CHOOSING, WHEEL, BOXES, ANSWER} from "../../model/status";
 
 const Main = () =>{
+    
+    const [gamestatus, setgamestatus] = useRecoilState(status);
+
     return (
         <div className="main">
+               { gamestatus == READY &&
+               <div>
             <div className = "danny">
                 <img src = {"../UI/drachenispy.png"} width = "210px" height = "250px"/>
             </div>
             <div className = "askbubble">
             <div className = "content">
-                    <p className = "introduction">
+                    <p className = "text">
                     Let’s learn more about AI in real life. But first, let’s choose a topic!
                     </p>
-                    <ButtonSmall>Confirm</ButtonSmall>
+                    <ButtonSmall onClick = {() => setgamestatus(CHOOSING)}>Confirm</ButtonSmall>
                 </div>
                 <img src = {"../UI/resbubble.png"} width = "600px" height = "600px" />
             </div>
-        </div>
+            </div>
+              }
+              {
+                gamestatus == CHOOSING &&
+                <div>
+                    <div className = "danny">
+                       <img src = {"../UI/drachenthinking.png"} width = "210px" height = "250px"/>
+                    </div>
+                    <div className = "smallaskbubble">
+                        <div className = "choosemodel">
+                              <p className = "text">
+                                Who is choosing today?
+                              </p>  
+                        </div>    
+                       <img src = {"../UI/resbubble.png"} width = "400px" height = "400px" />
+                    </div>  
+                    <div className = "choices">
+                        <Stack spacing = {10} direction = "column">
+                            <Button>DANI's Wheel</Button>
+                            <Button>Me</Button>
+                        </Stack>
+                    </div>      
+                </div>    
+              }
+        </div>      
     )
 };
 export default Main;
---
+++ src/model/status.ts
@@ -0,0 +1,5 @@
+export const READY = 0;
+export const CHOOSING = 1;
+export const WHEEL = 2;
+export const BOXES = 3;
+export const ANSWER = 4;

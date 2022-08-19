30104d7b52270bb8d2d08a78976476d0ce518bb5
actbee <rainman511@outlook.com>
2022-08-19 18:26:40 -0400

    updated part of the story pages

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -8,7 +8,8 @@ import React, { useState, useEffect, useRef }  from "react";
 import Stack from "@mui/material/Stack";
 import {status, words, parentname, childname} from "../../store";
 import {useRecoilState, useRecoilValue} from "recoil";
-import {READY, READY2, READY3, READY4, CHOOSING, WHEEL, BOXES, CHOOSING2, ANSWER, ANSWER2, ANSWER3, ANSWER4, LEARN, STORY, END} from "../../model/status";
+import {READY, READY2, READY3, READY4, CHOOSING, WHEEL, BOXES, CHOOSING2, 
+    ANSWER, ANSWER2, ANSWER3, ANSWER4, LEARN, STORY, STORY2, STORY3, STORY4, STORY5, END} from "../../model/status";
 import { intro1, introlist } from "../database/introductions";
 import { Wheel } from 'react-custom-roulette'
 
@@ -20,6 +21,7 @@ const Main = () =>{
     const [mustSpin, setMustSpin] = useState(false);
     const [topicnumber, settopicnumber] = useState(0);
     const [spined, setspined] = useState(false);
+    const [readstory, setreadstory] = useState(false);
     const parent = useRecoilValue(parentname);
     const child = useRecoilValue(childname);
     const message = new SpeechSynthesisUtterance();
@@ -249,6 +251,53 @@ const Main = () =>{
                    </div>
                    
             }
+               {
+                gamestatus == STORY &&
+                   <div>
+                         <div className = "danny">
+                       <img src = {"../UI/drachenispy.png"} width = "210px" height = "250px"/>
+                        </div>
+                        <div className = "askbubble-left">
+                            <div className = "content-big">
+                                <p id = "chatbox1" className = "text">
+                                   {introlist[0][0]}
+                                </p> 
+                                <p id = "chatbox" className = "text-small">
+                                In this part of the module, we are going to listen to a story and discuss some questions related to this topic.
+                                </p>
+                             </div>
+                             <img src = {"../UI/resbubble.png"} width = "600px" height = "600px" />
+                         </div>   
+                         <div className = "choices-down">
+                            <Button onClick = {() => setgamestatus(STORY2)}>Confirm</Button>
+                        </div>      
+                   </div>    
+            }
+              {
+                gamestatus == STORY2 &&
+                   <div>
+                         <div className = "danny">
+                       <img src = {"../UI/drachenispy.png"} width = "210px" height = "250px"/>
+                        </div>
+                        <div className = "askbubble-left">
+                            <div className = "content-big">
+                                <p id = "chatbox1" className = "text">
+                                   {introlist[0][0]}
+                                </p> 
+                                <p id = "chatbox" className = "text-small">
+                                Ok {parent}, could you please read the story to {child} and me? Or would you prefer I read the story out loud?
+                                </p>
+                             </div>
+                             <img src = {"../UI/resbubble.png"} width = "600px" height = "600px" />
+                         </div>   
+                         <div className = "choices-right">
+                        <Stack spacing = {5} direction = "column">
+                            <Button onClick = {() => {setgamestatus(STORY3); setreadstory(false)}}>I will read!</Button>
+                            <Button onClick = {() => {setgamestatus(STORY3); setreadstory(true)}}>Dani will read!</Button>
+                        </Stack>
+                        </div>    
+                   </div>    
+            }
             {
                 gamestatus == ANSWER4 &&
                    <div>
@@ -273,8 +322,59 @@ const Main = () =>{
                             <Button onClick = {() => setgamestatus(END)}>Exit Module</Button>
                         </Stack>
                         </div>      
-                   </div>
-                   
+                   </div>    
+            }
+              {
+                gamestatus == ANSWER3 &&
+                   <div>
+                         <div className = "danny">
+                       <img src = {"../UI/drachenispy.png"} width = "210px" height = "250px"/>
+                        </div>
+                        <div className = "askbubble-left">
+                            <div className = "content-big">
+                                <p id = "chatbox1" className = "text">
+                                   {introlist[0][0]}
+                                </p> 
+                                <p id = "chatbox" className = "text-small">
+                                Clicking the "Search" button will open a new browser for topic exploration.
+                                </p>
+                             </div>
+                             <img src = {"../UI/resbubble.png"} width = "600px" height = "600px" />
+                         </div>   
+                         <div className = "choices-right">
+                        <Stack spacing = {5} direction = "column">
+                            <Button onClick = {() => window.open("https://www.google.com/search?q=machine+learning+bias+&ei=hAgAY9KiB_qliLMP2deI-Ak&ved=0ahUKEwjS9MeP9NP5AhX6EmIAHdkrAp8Q4dUDCA4&uact=5&oq=machine+learning+bias+&gs_lcp=Cgdnd3Mtd2l6EAMyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEOgcIABBHELADOgYIABAeEBY6BQgAEIYDOggIABAeEA8QFkoECEEYAEoECEYYAFC8AliEBmDJCGgBcAF4AIABaIgB1wKSAQMzLjGYAQCgAQHIAQjAAQE&sclient=gws-wiz",'_blank')}>Search</Button>
+                            <Button onClick = {() => setgamestatus(ANSWER4)}>Exit</Button>
+                        </Stack>
+                        </div>      
+                   </div>    
+            }
+                 {
+                gamestatus == ANSWER2 &&
+                   <div>
+                         <div className = "danny">
+                       <img src = {"../UI/drachenispy.png"} width = "210px" height = "250px"/>
+                        </div>
+                        <div className = "askbubble-left">
+                            <div className = "content-big">
+                                <p id = "chatbox1" className = "text">
+                                   {introlist[0][0]}
+                                </p> 
+                                <p id = "chatbox" className = "text-small">
+                                Here are links to reputable sources about this topic! 
+                                </p>
+                             </div>
+                             <img src = {"../UI/resbubble.png"} width = "600px" height = "600px" />
+                         </div>   
+                         <div className = "choices-right">
+                        <Stack spacing = {3} direction = "column">
+                            <Button onClick = {() => window.open("https://www.vox.com/future-perfect/22916602/ai-bias-fairness-tradeoffs-artificial-intelligence",'_blank')}>Link1</Button>
+                            <Button onClick = {() => window.open("https://www.mckinsey.com/featured-insights/artificial-intelligence/tackling-bias-in-artificial-intelligence-and-in-humans",'_blank')}>Link2</Button>
+                            <Button onClick = {() => window.open("https://research.aimultiple.com/ai-bias/",'_blank')}>Link3</Button>
+                            <Button onClick = {() => setgamestatus(ANSWER4)}>Exit</Button>
+                        </Stack>
+                        </div>      
+                   </div>    
             }
             {
                 gamestatus == ANSWER &&
